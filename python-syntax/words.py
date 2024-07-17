8934eb6fed3d5268f6ae049a95c382df9f10f4a1
Taimoor Arshad <taimoor.arshad43@gmail.com>
2024-07-17 13:59:31 -0400

    Adding documentation

--- python-syntax/words.py
+++ python-syntax/words.py
@@ -1,4 +1,9 @@
 def print_upper_words(words, must_start_with = {}):
+
+    '''This function takes a list of words and returns them line by line in upper case
+        A second arguement specifies to only return words that start with specific characters
+        '''
+
     for word in words:
         for char in must_start_with:
             if word[0] == char:
