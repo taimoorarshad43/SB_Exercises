dfed14e681f3fee63f766164938c948c1598356d
Taimoor Arshad <taimoor.arshad43@gmail.com>
2025-07-30 20:52:25 -0400

    Deleting seed.sql file

--- flask/flaskintermediate/WTForms/seed.sql
+++
@@ -1,5 +0,0 @@
--- SQLite version of seed data
--- This file is kept for reference, but data will be created programmatically
-
--- The pets table will be created by SQLAlchemy
--- Sample data can be added programmatically if needed 
