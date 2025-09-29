c8d44489b2e16488cfb57c0421d4a1d69529a442
Taimoor Arshad <taimoor.arshad43@gmail.com>
2025-09-29 00:32:08 -0400

    Adding React pokemon

---
+++ react/react-cards-pokemon/src/PlayingCardList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {v1 as uuid} from "uuid";
+import { useAxios } from "./hooks";
+import PlayingCard from "./PlayingCard";
+import "./PlayingCardList.css";
+
+/* Renders a list of playing cards.
+ * Can also add a new card at random. */
+function CardTable() {
+  const [cards, addCard] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+  const addCardWithId = () => {
+    addCard(data => ({ ...data, id: uuid() }));
+  };
+  return (
+    <div className="PlayingCardList">
+      <h3>Pick a card, any card!</h3>
+      <div>
+        <button onClick={addCardWithId}>Add a playing card!</button>
+      </div>
+      <div className="PlayingCardList-card-area">
+        {cards.map(cardData => (
+          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+CardTable.defaultProps = {};
+
+export default CardTable;
